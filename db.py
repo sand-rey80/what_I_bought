ea88b3620e9b3033758db945dced8d3f16a23f7d
sand-rey80 <sand-rey80@ya.ru>
2024-11-15 22:27:08 +0300

    В Модель таблицы добавил столбец с Telegramm id отправителя

--- db.py
+++ db.py
@@ -2,16 +2,17 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, sessionmaker
 
 engine = create_async_engine("sqlite+aiosqlite:///sqdata.db")
-ses_maker = async_sessionmaker(engine)
+sessionmaker = async_sessionmaker(engine)
 
 class Model(DeclaratibeBase):
     __abstract__ = True
     id: Mapped[int] = mapped_column(primary_key=True)
-
+    created_at = Column(DateTime, default=func.now())
 
 class TicketsOrm(Model):
     __tablename__ = "tickets"
-    qr_value: Mapped[str]
+    qr_value: Mapped[str] = mapped_column(String(150))
+    tg_id: Mapped[str] = mapped_column(String(30))
 
 
 async def create_tables():
