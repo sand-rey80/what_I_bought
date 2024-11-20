9a87d5c32dd745932f006b64fd9cc23370320618
sand-rey80 <sand-rey80@ya.ru>
2024-11-21 01:24:03 +0300

    Добавил бота на полинге, чтобы без публикации работал

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import uvicorn
-from fastapi import FastAPI, Request
 import os
 from dotenv import load_dotenv
+import asyncio
+from aiogram import Bot, Dispatcher
+import uvicorn
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -12,17 +14,28 @@ app = FastAPI()
 
 @app.get("/")
 async def getget(req: Request):
-    print("А")
-    return {"ok":"o2"}
+    print("run get")
+    return {"get":"ok"}
 
 
 @app.post("/")
 async def telega():
-    print("1111111")
-    return {"req":"1"}
+    print("run post")
+    return {"post":"ok"}
+
+
+async def startBot():
+    bot = Bot(token=os.getenv('BOT_TOKEN'))
+    dp = Dispatcher()
+    await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
     load_dotenv()
+    try:
+        asyncio.run(startBot())
+    except KeyboardInterrupt:
+        pass
 #    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
 #    BOT_TOKEN = os.getenv('BOT_TOKEN')
 
